58eec54ea39efb311ad3db350b23c267e0a3b454
TIBIsuporte <ti.suporte.lens@gmail.com>
2026-08-19 20:37:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.post("/consulta", async (req, res) => {
   console.log("Body enviado para ProdutosPorOSGrid:", body);
 
   try {
-    const response = await fetch("https://api.savwinweb.com.br/api/APIRelatoriosCR/ProdutosPorOSGrid", {
+    //const response = await fetch("https://api.savwinweb.com.br/api/APIRelatoriosCR/ProdutosPorOSGrid", {
+    const response = await fetch("https://api.savwinweb.com.br/api/APIDados/RetornaVendasPendentesCompletas{
+      
       method: "POST",
       headers: {
         "Content-Type": "application/json",
