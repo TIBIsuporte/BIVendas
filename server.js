f6315d45c2ae7fa055f5f3f46274e6e58a1d0b4a
TIBIsuporte <ti.suporte.lens@gmail.com>
2026-08-19 20:32:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 
 // Servir a página index.html e arquivos estáticos da pasta
